ebf18d3e6b29ad0a87fd09612ea8bd527bfe1590
Josh Davidson <jdavidson@621.solutions>
2020-11-18 17:26:17 -0500

    Updates to Jackett cdk8s example for persistent volume claims.

--- cdk8s-examples/jackett.ts
+++ cdk8s-examples/jackett.ts
@@ -1,45 +1,65 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class JackettChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('jackett.lan', this.getIngressBackend());
-  }
+    const label = {app: 'jackett'};
 
-  private static getContainer() {
-    const container = new kplus.Container( {
-      image: 'linuxserver/jackett',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 9117,
-      volumeMounts:[{
-        path: '/config',
-        volume: kplus.Volume.fromEmptyDir('config'),
-      }]
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'jackett'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
+        }
+      }
+    });
+    
+    const service = new Service(this, 'service', {
+      ports: [{port: 9117, targetPort: 9117}]
+    });
+    service.addSelector('app', 'jackett');
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [{name: 'jackett', persistentVolumeClaim: {claimName: 'jackett'}}],
+            containers: [{
+              name: 'jackett',
+              image: 'linuxserver/jackett',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 9117}],
+              env: [
+                {name: 'PUID', value: '1000'},
+                {name: 'PGID', value: '1000'},
+                {name: 'TZ', value: 'America/New_York'},
+                {name: 'AUTO_UPDATE', value: 'true'}
+              ],
+              volumeMounts: [{mountPath: '/config', name: 'jackett'}]
+            }]
+          }
+        }
+      }
     });
 
-    container.addEnv('PUID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('PGID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('TZ',   kplus.EnvValue.fromValue('America/New_York'));
-    container.addEnv('AUTO_UPDATE',   kplus.EnvValue.fromValue('true'));
-
-
-    return container;
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('jackett.lan', IngressBackend.fromService(service));
   }
-
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [ JackettChart.getContainer() ]
-    });
-  }
-
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(9117));
-  }
-
 }
 
 const app = new App();
