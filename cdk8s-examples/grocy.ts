98479440e36f552d6c311f532e6d23946cec8545
Josh Davidson <jdavidson@621.solutions>
2020-11-18 16:36:33 -0500

    Updates to Grocy cdk8s example for persistent volume claims.

--- cdk8s-examples/grocy.ts
+++ cdk8s-examples/grocy.ts
@@ -1,43 +1,64 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class GrocyChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('grocy.lan', this.getIngressBackend());
-  }
+    const label = {app: 'grocy'};
 
-  private static getContainer() {
-    const container = new kplus.Container( {
-      image: 'linuxserver/grocy',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 80,
-      volumeMounts:[{
-        path: '/config',
-        volume: kplus.Volume.fromEmptyDir('config'),
-      }]
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'grocy'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
+        }
+      }
+    });
+    
+    const service = new Service(this, 'service', {
+      ports: [{port: 80, targetPort: 80}]
+    });
+    service.addSelector('app', 'grocy');
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [{name: 'grocy', persistentVolumeClaim: {claimName: 'grocy'}}],
+            containers: [{
+              name: 'grocy',
+              image: 'linuxserver/grocy',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 80}],
+              env: [
+                {name: 'PUID', value: '1000'},
+                {name: 'PGID', value: '1000'},
+                {name: 'TZ', value: 'America/New_York'}
+              ],
+              volumeMounts: [{mountPath: '/config', name: 'grocy'}]
+            }]
+          }
+        }
+      }
     });
 
-    container.addEnv('PUID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('PGID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('TZ',   kplus.EnvValue.fromValue('America/New_York'));
-
-    return container;
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('grocy.lan', IngressBackend.fromService(service));
   }
-
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [ GrocyChart.getContainer() ]
-    });
-  }
-
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(80));
-  }
-
 }
 
 const app = new App();
