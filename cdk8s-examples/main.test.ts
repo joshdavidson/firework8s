68252c4a2f4dfeb89be8e288ef5fef1fe5f06585
Josh Davidson <jdavidson@621.solutions>
2020-11-18 12:56:00 -0500

    Adding minecraft bedrock server to cdk8s examples

--- cdk8s-examples/main.test.ts
+++ cdk8s-examples/main.test.ts
@@ -19,6 +19,7 @@ import {LazyLibrarianChart} from "./lazylibrarian";
 import {LidarrChart} from "./lidarr";
 import {MariaDbChart} from "./mariadb";
 import {MinecraftChart} from "./minecraft";
+import {MinecraftBedrockChart} from "./minecraft-bedrock";
 import {StuffInSpaceChart} from "./stuffinspace";
 import {VsCodeChart} from "./vscode";
 import {WallabagChart} from "./wallabag";
@@ -142,6 +143,12 @@ describe('Placeholder', () => {
   test('Minecraft', () => {
     const app = Testing.app();
     const chart = new MinecraftChart(app, 'test-minecraft');
+    const results = Testing.synth(chart)
+    expect(results).toMatchSnapshot();
+  });
+  test('MinecraftBedrock', () => {
+    const app = Testing.app();
+    const chart = new MinecraftBedrockChart(app, 'test-minecraft-bedrock');
     const results = Testing.synth(chart)
     expect(results).toMatchSnapshot();
   });
---
+++ cdk8s-examples/minecraft-bedrock.ts
@@ -0,0 +1,76 @@
+import { Construct } from 'constructs';
+import { App, Chart } from 'cdk8s';
+import { Deployment, IntOrString, PersistentVolumeClaim, Service } from './imports/k8s'
+
+export class MinecraftBedrockChart extends Chart {
+
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+    const label = { app: 'minecraft-bedrock' };
+
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'minecraft-bedrock'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '2Gi'
+          }
+        }
+      }
+    });
+
+    new Service(this, 'service', {
+      spec: {
+        type: 'NodePort',
+        ports: [ { port: 19132, nodePort: 19132, targetPort: IntOrString.fromNumber(19132), protocol: 'UDP' } ],
+        selector: label,
+      }
+    });
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            volumes:[{
+              name: 'data',
+              persistentVolumeClaim:{claimName: 'minecraft-bedrock'}
+            }],
+            containers: [{
+                name: 'minecraft-bedrock',
+                image: 'itzg/minecraft-bedrock-server',
+                imagePullPolicy: 'Always',
+                env: [
+                    { name: 'EULA', value: 'true' },
+                    { name: 'GAMEMODE', value: 'creative' },
+                    { name: 'DIFFICULTY', value: 'easy' }
+                ],
+                ports: [{containerPort: 19132, hostPort: 19132, protocol: 'UDP'}],
+                volumeMounts: [{mountPath: '/data', name: 'data'}],
+                readinessProbe:{
+                  exec:{
+                    command: ['mc-monitor', 'status-bedrock', '-- host', '127.0.0.1']
+                  },
+                  initialDelaySeconds: 5,
+                  periodSeconds: 5
+                }
+              }]
+          }
+        }
+      }
+    });
+
+  }
+}
+
+const app = new App();
+new MinecraftBedrockChart(app, 'minecraft');
+app.synth();
