4a2dc411715ac4f618d2ddfcb1af3b06ad5844e4
Josh Davidson <jdavidson@621.solutions>
2020-11-17 21:06:01 -0500

    Adding Bookstack

--- cdk8s-examples/main.test.ts
+++ cdk8s-examples/main.test.ts
@@ -3,11 +3,18 @@ import {BitwardenChart} from "./bitwarden";
 import {CyberChefChart} from "./cyberchef";
 import {HeimdallChart} from "./heimdall";
 import {VsCodeChart} from "./vscode";
+import {BookstackChart} from "./bookstack";
 
 describe('Placeholder', () => {
   test('Bitwarden', () => {
     const app = Testing.app();
     const chart = new BitwardenChart(app, 'test-bitwarden');
+    const results = Testing.synth(chart)
+    expect(results).toMatchSnapshot();
+  });
+  test('Bookstack', () => {
+    const app = Testing.app();
+    const chart = new BookstackChart(app, 'test-bookstack');
     const results = Testing.synth(chart)
     expect(results).toMatchSnapshot();
   });
--- cdk8s-examples/main.ts
+++ cdk8s-examples/main.ts
@@ -1,11 +1,13 @@
 import { App } from 'cdk8s';
 import {BitwardenChart} from "./bitwarden";
+import {BookstackChart} from "./bookstack";
 import {CyberChefChart} from "./cyberchef";
 import {HeimdallChart} from "./heimdall";
 import {VsCodeChart} from "./vscode";
 
 const app = new App();
 new BitwardenChart(app, 'bitwarden');
+new BookstackChart(app, 'bookstack');
 new CyberChefChart(app, 'cyberchef');
 new HeimdallChart(app, 'heimdall');
 new VsCodeChart(app, 'vscode');
