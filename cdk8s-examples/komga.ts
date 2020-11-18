120133d7768359d7d6246a3a59ea3bdfe87c7633
Josh Davidson <jdavidson@621.solutions>
2020-11-18 17:39:42 -0500

    Updates to Komga cdk8s example for persistent volume claims.

--- cdk8s-examples/komga.ts
+++ cdk8s-examples/komga.ts
@@ -1,45 +1,70 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class KomgaChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('komga.lan', this.getIngressBackend());
-  }
+    const label = {app: 'komga'};
 
-  private static getContainer() {
-    return new kplus.Container( {
-      name: 'komga',
-      image: 'gotson/komga',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 8080,
-      args:[ '--user 1000:1000' ],
-      volumeMounts:[
-        {
-          path: '/config',
-          volume: kplus.Volume.fromEmptyDir('config')
-        },
-        {
-          path: '/comics',
-          volume: kplus.Volume.fromEmptyDir('comics')
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'komga'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
         }
-      ]
+      }
     });
-  }
-
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [ KomgaChart.getContainer() ]
+    
+    const service = new Service(this, 'service', {
+      ports: [{port: 8080, targetPort: 8080}]
     });
-  }
+    service.addSelector('app', 'komga');
 
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(8080));
-  }
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [
+              {name: 'komga', persistentVolumeClaim: {claimName: 'komga'}},
+              {name: 'comics', hostPath: {path: '/mnt/share/Comics'}}
+            ],
+            containers: [{
+              name: 'komga',
+              image: 'gotson/komga',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 8080}],
+              env: [
+                {name: 'PUID', value: '1000'},
+                {name: 'PGID', value: '1000'},
+                {name: 'TZ', value: 'America/New_York'}
+              ],
+              volumeMounts: [
+                {mountPath: '/config', name: 'komga'},
+                {mountPath: '/comics', name: 'comics'}
+              ]
+            }]
+          }
+        }
+      }
+    });
 
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('komga.lan', IngressBackend.fromService(service));
+  }
 }
 
 const app = new App();
