729800dc24ce39839761470d9b790d24fa53033d
Josh Davidson <jdavidson@621.solutions>
2020-11-18 17:59:16 -0500

    Updates to Wallabag cdk8s example for persistent volume claims.

--- cdk8s-examples/wallabag.ts
+++ cdk8s-examples/wallabag.ts
@@ -1,42 +1,63 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class WallabagChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('wallabag.lan', this.getIngressBackend());
-  }
+    const label = {app: 'wallabag'};
 
-  private static getContainer() {
-    const container = new kplus.Container( {
-      image: 'wallabag/wallabag',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 80,
-      volumeMounts:[{
-        path: '/var/www/wallabag/data',
-        volume: kplus.Volume.fromEmptyDir('data'),
-      }]
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'wallabag'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
+        }
+      }
+    });
+    
+    const service = new Service(this, 'service', {
+      ports: [{port: 80, targetPort: 80}]
+    });
+    service.addSelector('app', 'wallabag');
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [{name: 'wallabag', persistentVolumeClaim: {claimName: 'wallabag'}}],
+            containers: [{
+              name: 'wallabag',
+              image: 'wallabag/wallabag',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 80}],
+              env: [
+                {name: 'SYMFONY__ENV__DOMAIN_NAME', value: 'http://wallabag.lan'},
+                {name: 'POPULATE_DATABASE', value: 'True'},
+              ],
+              volumeMounts: [{mountPath: '/var/www/wallabag/data', name: 'wallabag'}]
+            }]
+          }
+        }
+      }
     });
 
-    container.addEnv('SYMFONY__ENV__DOMAIN_NAME', kplus.EnvValue.fromValue('http://wallabag.lan'));
-    container.addEnv('POPULATE_DATABASE', kplus.EnvValue.fromValue('True'));
-
-    return container;
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('wallabag.lan', IngressBackend.fromService(service));
   }
-
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [ WallabagChart.getContainer() ]
-    });
-  }
-
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(80));
-  }
-
 }
 
 const app = new App();
