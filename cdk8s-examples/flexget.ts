60c63a2a58c07c8ae78f2afb8fcd7713d9dcf0bc
Josh Davidson <jdavidson@621.solutions>
2020-11-18 16:16:17 -0500

    Updates to FlexGet cdk8s example for persistent volume claims.

--- cdk8s-examples/flexget.ts
+++ cdk8s-examples/flexget.ts
@@ -1,49 +1,93 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class FlexGetChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('flexget.lan', this.getIngressBackend());
-  }
+    const label = {app: 'flexget'};
 
-  private static getContainer() {
-    const container = new kplus.Container( {
-      image: 'wiserain/flexget',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 5050,
-      volumeMounts:[
-        { path: '/config',
-          volume: kplus.Volume.fromEmptyDir('config')
-        },
-        { path: '/data',
-          volume: kplus.Volume.fromEmptyDir('data')
+    new PersistentVolumeClaim(this, 'flexget-config', {
+      metadata: {
+        name: 'flexget-config'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
         }
-      ]
+      }
     });
 
-    container.addEnv('PUID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('PGID', kplus.EnvValue.fromValue('1000'));
-    container.addEnv('TZ',   kplus.EnvValue.fromValue('America/New_York'));
-    container.addEnv('FG_WEBUI_PASSWD',   kplus.EnvValue.fromValue('password'));
-    container.addEnv('FG_LOG_LEVEL',   kplus.EnvValue.fromValue('info'));
-
-    return container;
-  }
-
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [ FlexGetChart.getContainer() ]
+    new PersistentVolumeClaim(this, 'flexget-data', {
+      metadata: {
+        name: 'flexget-data'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
+        }
+      }
     });
-  }
 
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(5050));
-  }
+    const service = new Service(this, 'service', {
+      ports: [{port: 5050, targetPort: 5050}]
+    });
+    service.addSelector('app', 'flexget');
 
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [
+              {name: 'flexget-config', persistentVolumeClaim: {claimName: 'flexget-config'}},
+              {name: 'flexget-data', persistentVolumeClaim: {claimName: 'flexget-data'}},
+              {name: 'downloads', hostPath: {path: '/mnt/share/Downloads'}},
+              {name: 'movies', hostPath: {path: '/mnt/share/Movies'}},
+              {name: 'tv', hostPath: {path: '/mnt/share/Television'}}
+            ],
+            containers: [{
+              name: 'flexget',
+              image: 'wiserain/flexget',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 5050}],
+              env: [
+                {name: 'PUID', value: '1000'},
+                {name: 'PGID', value: '1000'},
+                {name: 'TZ', value: 'America/New_York'},
+                {name: 'FG_WEBUI_PASSWD', value: 'password'},
+                {name: 'FG_LOG_LEVEL', value: 'info'}
+              ],
+              volumeMounts: [
+                {mountPath: '/config', name: 'flexget-config'},
+                {mountPath: '/data', name: 'flexget-data'},
+                {mountPath: '/downloads', name: 'downloads'},
+                {mountPath: '/movies', name: 'movies'},
+                {mountPath: '/tv', name: 'tv'}
+              ]
+            }]
+          }
+        }
+      }
+    });
+
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('flexget.lan', IngressBackend.fromService(service));
+  }
 }
 
 const app = new App();
