6cd0a474dc8e53f63a82ed25b1b7fecf8059fbce
Josh Davidson <jdavidson@621.solutions>
2020-11-18 16:22:31 -0500

    Updates to Gaps cdk8s example for persistent volume claims.

--- cdk8s-examples/gaps.ts
+++ cdk8s-examples/gaps.ts
@@ -1,37 +1,59 @@
-import {Construct} from 'constructs';
-import {App, Chart} from 'cdk8s';
-import * as kplus from 'cdk8s-plus';
+import { Construct } from 'constructs';
+import { App, Chart } from 'cdk8s';
+import { Deployment, PersistentVolumeClaim } from './imports/k8s'
+import {Ingress, IngressBackend, Service} from 'cdk8s-plus';
 
 export class GapsChart extends Chart {
 
   constructor(scope: Construct, name: string) {
     super(scope, name);
-    const ingress = new kplus.Ingress(this, 'ingress');
-    ingress.addHostDefaultBackend('gaps.lan', this.getIngressBackend());
-  }
+    const label = {app: 'gaps'};
 
-  private static getContainer() {
-    return new kplus.Container( {
-      image: 'housewrecker/gaps',
-      imagePullPolicy: kplus.ImagePullPolicy.ALWAYS,
-      port: 8484,
-      volumeMounts:[{
-        path: '/usr/data',
-        volume: kplus.Volume.fromEmptyDir('data'),
-      }]
+    new PersistentVolumeClaim(this, 'pvc', {
+      metadata: {
+        name: 'gaps'
+      },
+      spec: {
+        storageClassName: 'default',
+        accessModes: ['ReadWriteOnce'],
+        resources: {
+          requests: {
+            storage: '250Mi'
+          }
+        }
+      }
     });
-  }
 
-  private getDeployment() {
-    return new kplus.Deployment(this, 'deployment', {
-      containers: [GapsChart.getContainer()]
+    const service = new Service(this, 'service', {
+      ports: [{port: 8484, targetPort: 8484}]
     });
-  }
+    service.addSelector('app', 'gaps');
 
-  private getIngressBackend() {
-    return kplus.IngressBackend.fromService(this.getDeployment().expose(8484));
-  }
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: {labels: label},
+          spec: {
+            volumes: [{name: 'gaps', persistentVolumeClaim: {claimName: 'gaps'}}],
+            containers: [{
+              name: 'gaps',
+              image: 'housewrecker/gaps',
+              imagePullPolicy: 'Always',
+              ports: [{containerPort: 8484}],
+              volumeMounts: [{mountPath: '/usr/data', name: 'gaps'}]
+            }]
+          }
+        }
+      }
+    });
 
+    const ingress = new Ingress(this, 'ingress');
+    ingress.addHostDefaultBackend('gaps.lan', IngressBackend.fromService(service));
+  }
 }
 
 const app = new App();
